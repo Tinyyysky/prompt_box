ae8b4c889d35948533f43459722bb3075dd65dc6
Tinyyysky <1815323423@qq.com>
2026-03-22 23:06:22 +0800

    revert: reset MainActivity.java to v1.0.2.2 (before status bar changes)

--- app/src/main/java/com/promptbox/app/MainActivity.java
+++ app/src/main/java/com/promptbox/app/MainActivity.java
@@ -62,8 +62,8 @@ public class MainActivity extends Activity {
                 String js = "(function(){" +
                     "var s=document.createElement('style');" +
                     "s.textContent='" +
-                    ".hdr{padding-top:35px !important}" +
-                    ".search{padding-top:35px !important}" +
+                    ".sb-hd{padding-top:max(50px," + sb + "px) !important}" +
+                    ".hdr{padding-top:calc(" + sb + "px + 12px) !important}" +
                     ".batch-bar{bottom:max(20px," + nb + "px) !important}" +
                     ".m-ft{padding-bottom:max(12px," + nb + "px) !important}" +
                     ".fab{bottom:calc(max(20px," + nb + "px) + 8px) !important}" +
